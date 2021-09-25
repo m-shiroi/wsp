866623c51148317c1102495d9bdaf86730953f51
m-shiroi <makkusu.shiroi@gmail.com>
2021-09-25 09:30:01 +0300

    vim: Add installation script

---
+++ vim/install.sh
@@ -0,0 +1,22 @@
+#! /bin/env bash
+
+declare -r REMOTE="remote"
+declare -r SYNC_FILE="${HOME}/.vimrc"
+declare -r SYNC_FOLDER="${HOME}/.vim"
+
+main() {
+	local inst_file="${PWD}/vimrc"
+	local inst_folder="${PWD}/vim"
+
+	if [ ${inst_file} -ef  ${SYNC_FILE} ]; then
+		printf "(already)"
+	else
+		rm -rf ${SYNC_FILE} ${SYNC_FOLDER}
+		ln -s ${inst_folder} ${SYNC_FOLDER}
+		ln -s ${inst_file} ${SYNC_FILE}
+	fi
+
+	echo "SET-UP: vim"
+}
+
+main $@
