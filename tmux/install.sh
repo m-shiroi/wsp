55f379305cf8ed60ef77e4e0c1fbc224f1e5604b
m-shiroi <makkusu.shiroi@gmail.com>
2021-09-25 09:35:05 +0300

    tmux: Simplify logic in installation script

--- tmux/install.sh
+++ tmux/install.sh
@@ -5,14 +5,14 @@ declare -r SYNC_FILE="${HOME}/.tmux.conf"
 
 main() {
 	local inst_type=${1}
-	local inst_file="tmux.conf_m"
+	local inst_file="${PWD}/tmux.conf_m"
 
-	[ "${inst_type}" == "${REMOTE}" ] && inst_file="tmux.conf_r"
+	[ "${inst_type}" == "${REMOTE}" ] && inst_file="${PWD}/tmux.conf_r"
 	if [ ${inst_file} -ef  ${SYNC_FILE} ]; then
 		printf "(already)"
 	else
 		rm ${SYNC_FILE}
-		ln -s $(pwd)/${inst_file} ${SYNC_FILE}
+		ln -s ${inst_file} ${SYNC_FILE}
 	fi
 	echo "SET-UP: tmux"
 }
