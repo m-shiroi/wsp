afc8ea17bda754ea65c2bd245cbd73d62369aaab
m-shiroi <makkusu.shiroi@gmail.com>
2021-09-25 09:32:03 +0300

    terminator: Add installation script

---
+++ terminator/install.sh
@@ -0,0 +1,20 @@
+#! /bin/env bash
+
+declare -r REMOTE="remote"
+declare -r SYNC_FONTS="${HOME}/.fonts/terminator_fonts"
+declare -r SYNC_TERMINAL="${HOME}/.config/terminator"
+
+main() {
+	local inst_terminal="${PWD}/terminator"
+	local inst_fonts="${PWD}/fonts"
+	if [ ${inst_terminal} -ef  ${SYNC_TERMINAL} ] && [ ${inst_fonts} -ef ${SYNC_FONTS} ]; then
+		printf "(already)"
+	else
+		rm -rf ${SYNC_FONTS} ${SYNC_TERMINAL}
+		ln -s ${inst_fonts} ${SYNC_FONTS}
+		ln -s ${inst_terminal} ${SYNC_TERMINAL}
+	fi
+	echo "SET-UP: terminator"
+}
+
+main $@
